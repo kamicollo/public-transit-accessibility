775d37260b1af375c1e146c4324bdc06ae7789b9
slazien <p.zientala.1995@gmail.com>
2022-04-22 19:51:44 -0400

    added Dockerfile for postgres

---
+++ DockerFiles/restore_dump.sh
@@ -0,0 +1,9 @@
+if [ "$DO_RESTORE" = true ] ; then \
+    echo "restoring from dump..." \
+    && psql -c "CREATE DATABASE main;" \
+    && psql -U postgres -d main < /data/main.sql \
+    ; \
+    else \
+    echo "not restoring from dump" \
+    ; \
+    fi
